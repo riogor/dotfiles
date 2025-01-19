60fdc7edac11abd94db678896190a7f0174c8f4e
riogor <astronaft100@yandex.ru>
2025-01-19 11:36:40 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#YAY
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+
+#OH-MY-ZSH
+yay -S zsh curl
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+cp .zshrc ~/.zshrc
+
+
+#VIM 
+cp .vimrc ~/.vimrc
+
+
+#I3 & CONFIG
+yay -S i3-wm i3blocks i3status i3-lock-color xss-lock rofi \
+	nm-applet numlockx \
+	pulseaudio brightnessctl \
+	feh picom redshift \
+	nautilus alacritty terminator 
+
+cp -r ./i3 ~/.config/i3
+cp -r ./i3status ~/.config/i3status
+
+
+#XORG CONFIGS
+read -p "Copy xorg configuration files (check devices)? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+		sudo cp -r ./xorg.conf.d /etc/X11/xorg.conf.d
+		sudo cp -r ./nvidia.xorg.conf.d /etc/X11/nvidia.xorg.conf.d
+fi
+
